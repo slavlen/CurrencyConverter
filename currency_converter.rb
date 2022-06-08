9f4a6198750273dddce6ac0de4d1c06728ce8801
Slavlen <slavlen2001@gmail.com>
2022-06-08 23:45:29 +0300

    first commit

---
+++ currency_converter.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+puts 'Of what? 1. Ruble 2. Dollar
+
+puts 'Сколько сейчас стоит 1 ${currency}?'
+usd_rate = gets.to_f
+
+if currency == '1'
+  puts 'How many rubles?'
+  rub = gets.to_f
+  usd = (rub / usd_rate).round(2)
+  puts 'Stocks are equal: ' + usd.to_s + ' $'
+else
+  puts 'How much dollars?'
+  usd = gets.to_f
+  rub = (usd * usd_rate).round(2)
+  puts 'Stocks are equal: ' + rub.to_s + ' руб.'
+end
